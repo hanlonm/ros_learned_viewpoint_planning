e52c0cc30be7907c843892d23819d2aeced55ea6
Matthew Hanlon <hanlonm@ethz.ch>
2023-07-06 18:42:55 +0200

    initial commit

---
+++ learned_viewpoint_planning/viewpoint_planner.py
@@ -0,0 +1,10 @@
+import rclpy
+from viewpoint_planning.planning.viewpoint_planning import HlocViewpointPlanner
+
+
+def main():
+    print('Hi from learned_viewpoint_planning.')
+
+
+if __name__ == '__main__':
+    main()
