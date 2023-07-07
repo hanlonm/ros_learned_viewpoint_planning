7e0a7e094838ac57bf61b8112714ca09e44b9b27
Matthew Hanlon <hanlonm@ethz.ch>
2023-07-07 16:49:06 +0200

    working on docker image

--- learned_viewpoint_planning/viewpoint_planner.py
+++ learned_viewpoint_planning/viewpoint_planner.py
@@ -1,9 +1,32 @@
+from typing import List
 import rclpy
+from rclpy.context import Context
+from rclpy.node import Node
+
 from viewpoint_planning.planning.viewpoint_planning import HlocViewpointPlanner
 
+from rclpy.parameter import Parameter
 
-def main():
+
+
+class ViewpointPlanningNode(Node):
+    def __init__(self) -> None:
+        super().__init__("viewpoint_planner")
+        print('Hi from the viewpoint planner.')
+        
+
+def main(args=None):
+    rclpy.init(args=args)
     print('Hi from learned_viewpoint_planning.')
+
+    viewpoint_planner = ViewpointPlanningNode()
+
+    rclpy.spin(viewpoint_planner)
+
+    viewpoint_planner.destroy_node()
+    rclpy.shutdown()
+
+    
 
 
 if __name__ == '__main__':
